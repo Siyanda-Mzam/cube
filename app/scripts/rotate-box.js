bbde930982d3c86f3531bbddbbf04d6551a9fa68
Siyanda Mzam <siyanda.mzam@hellocomputer.com>
2017-03-20 16:52:52 +0200

    Shortened the code and made it a bit cleaner

--- app/scripts/rotate-box.js
+++ app/scripts/rotate-box.js
@@ -1,48 +1,24 @@
 ; var isSet = 0;
-$(document).ready(function(){
-  $(".btn").click(function() {
-    $(this).each(function(){
-      if ($(this).hasClass("show-front")) {
-        $("#cube").css({transform: "rotateY(0deg)"});
-      }
-      else if ($(this).hasClass("show-back")) {
-          $("#cube").css({transform: "translateZ(-100px) rotateX(180deg)"});
-      }
-      else if ($(this).hasClass("show-top")) {
-        $("#cube").css({transform: "translateZ(-100px) rotateX(90deg)"});
-      }
-      else if ($(this).hasClass("show-bottom")) {
-        $("#cube").css({transform: "translateZ(-100px) rotateX(-90deg)"});
-      }
-      else if ($(this).hasClass("show-right")) {
-        $("#cube").css({transform: "translateZ(-100px) rotateY(-90deg)"});
-      }
-      else if ($(this).hasClass("show-left")) {
-        $("#cube").css({transform: "translateZ(-100px) rotateY(90deg)"});
-      }
-    });
- });
  $(".btn").click(function() {
-    $(this).each(function(){
-      if ($(this).hasClass("show-front")) {
+      var target = $(this);
+      if (target.attr("id") == "front") {
         $("#cube").css({transform: "rotateY(0deg)"});
       }
-      else if ($(this).hasClass("show-back")) {
+      else if (target.attr("id") == "back") {
           $("#cube").css({transform: "translateZ(-100px) rotateX(180deg)"});
       }
-      else if ($(this).hasClass("show-top")) {
+      else if (target.attr("id") == "top") {
         $("#cube").css({transform: "translateZ(-100px) rotateX(90deg)"});
       }
-      else if ($(this).hasClass("show-bottom")) {
+      else if (target.attr("id") == "bottom") {
         $("#cube").css({transform: "translateZ(-100px) rotateX(-90deg)"});
       }
-      else if ($(this).hasClass("show-right")) {
+      else if (target.attr("id") == "right") {
         $("#cube").css({transform: "translateZ(-100px) rotateY(-90deg)"});
       }
-      else if ($(this).hasClass("show-left")) {
+      else if (target.attr("id") == "left") {
         $("#cube").css({transform: "translateZ(-100px) rotateY(90deg)"});
       }
-    });
  });
   $("#toggle-backface-visibility").click(function() {
         if (isSet) {
@@ -58,4 +34,3 @@ $(document).ready(function(){
           });
         }
     });
-});
